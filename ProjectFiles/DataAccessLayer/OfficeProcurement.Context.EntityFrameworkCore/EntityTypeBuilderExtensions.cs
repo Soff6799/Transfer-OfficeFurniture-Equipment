49ef50074c788352b021909cf1d5a92f2268805f
Sofia Manzhosova <sonf43@mail.ru>
2026-08-03 18:22:21 +0300

    Конфигурация для Id (Extensions)

---
+++ ProjectFiles/DataAccessLayer/OfficeProcurement.Context.EntityFrameworkCore/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OfficeProcurement.Dal.Contracts;
+
+namespace OfficeProcurement.Context.EntityFrameworkCore;
+
+public static class EntityTypeBuilderExtensions
+{
+    /// <summary>
+    /// Задаёт конфигурацию ключа для идентификатора <see cref="IEntityWithId"/>
+    /// </summary>
+    public static void HasIdAsKey<T>(this EntityTypeBuilder<T> builder)
+        where T : class, IEntityWithId
+        => builder.HasKey(x => x.Id);
+}
