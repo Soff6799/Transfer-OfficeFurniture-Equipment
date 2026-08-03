f56e32183ce03b289f48d39a7da5bf1c6e10feeb
Sofia Manzhosova <sonf43@mail.ru>
2026-08-03 18:41:54 +0300

    Конфигурация свойств для UpdatedAt/UpdatedBy (Extensions)

--- ProjectFiles/DataAccessLayer/OfficeProcurement.Context.EntityFrameworkCore/EntityTypeBuilderExtensions.cs
+++ ProjectFiles/DataAccessLayer/OfficeProcurement.Context.EntityFrameworkCore/EntityTypeBuilderExtensions.cs
@@ -3,22 +3,35 @@ using OfficeProcurement.Dal.Contracts;
 
 namespace OfficeProcurement.Context.EntityFrameworkCore;
 
+/// <summary>
+/// Методы расширения для <see cref="EntityTypeBuilder"/>
+/// </summary>
 public static class EntityTypeBuilderExtensions
 {
     /// <summary>
-    /// Задаёт конфигурацию ключа для идентификатора <see cref="IEntityWithId"/>
+    /// Задаёт конфигурацию первичного ключа для идентификатора <see cref="IEntityWithId"/>
     /// </summary>
     public static void HasIdAsKey<T>(this EntityTypeBuilder<T> builder)
         where T : class, IEntityWithId
         => builder.HasKey(x => x.Id);
 
     /// <summary>
-    /// Задаёт конфигурайию свойств аудита добавления для сущности <inheritdoc cref="BaseAuditEntity"/>
+    /// Задаёт конфигурацию свойств аудита добавления для сущности <see cref="IEntityAuditCreated"/>
     /// </summary>
     public static void CreateAuditConfiguration<T>(this EntityTypeBuilder<T> builder)
         where T : class, IEntityAuditCreated
     {
         builder.Property(x => x.CreatedAt).IsRequired();
         builder.Property(x => x.CreatedBy).IsRequired().HasMaxLength(200);
+    }
+
+    /// <summary>
+    /// Задаёт конфигурацию свойств полного аудита для сущности <see cref="IEntityAuditUpdate"/>
+    /// </summary>
+    public static void UpdateAuditConfiguration<T>(this EntityTypeBuilder<T> builder)
+        where T : class, IEntityAuditUpdate
+    {
+        builder.Property(x => x.UpdatedAt).IsRequired();
+        builder.Property(x => x.UpdatedBy).IsRequired().HasMaxLength(200);
     }
 }
