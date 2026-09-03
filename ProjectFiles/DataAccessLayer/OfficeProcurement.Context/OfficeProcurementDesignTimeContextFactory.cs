ea1460eca4e5b9d40d90c842137cecafeba6538e
Sofia Manzhosova <sonf43@mail.ru>
2026-09-03 18:45:09 +0300

    Add: библиотеки для работы с PostgreSQL, строка подключения и инструкция

--- ProjectFiles/DataAccessLayer/OfficeProcurement.Context/OfficeProcurementDesignTimeContextFactory.cs
+++ ProjectFiles/DataAccessLayer/OfficeProcurement.Context/OfficeProcurementDesignTimeContextFactory.cs
@@ -1,6 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
 namespace OfficeProcurement.Context;
 
-public class OfficeProcurementDesignTimeContextFactory
+
+public class OfficeProcurementDesignTimeContextFactory : IDesignTimeDbContextFactory<OfficeProcurementContext>
 {
-    
+    public OfficeProcurementContext CreateDbContext(string[] args)
+    {
+        var connectionString = "Host=localhost;Port=5432;Database=OfficeProcurement;Username=postgres;Password=Qwerty54321";
+        var options = new DbContextOptionsBuilder<OfficeProcurementContext>()
+            .UseNpgsql(connectionString)
+            .LogTo(Console.WriteLine)
+            .Options;
+
+        return new OfficeProcurementContext(options);
+
+    }
+
 }
